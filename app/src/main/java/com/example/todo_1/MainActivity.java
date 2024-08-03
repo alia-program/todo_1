7486c19125e95ebb9a82b26429bc0db18a000529
risan <aliaillust.program@gmail.com>
2024-08-03 10:27:15 +0900

    シリアライズ失敗 ・増殖 ・二行目以降の内容が保存できない

--- app/src/main/java/com/example/todo_1/MainActivity.java
+++ app/src/main/java/com/example/todo_1/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.todo_1;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +12,7 @@ import android.widget.LinearLayout;
 
 import org.json.JSONException;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -28,10 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<TodoView> todoViewArrayList = new ArrayList<>();
     ArrayList<Cos_EditText> editList = new ArrayList<>();
+    ArrayList<String> content = new ArrayList<>();
     ArrayList DataSet[] = {todoViewArrayList,editList};
 
-    int index = editList.size();
-    String fileName = "MEMOdata.dat";
+    int index;
+    File fileName = new File("MEMOdata.dat");
 
 
     @Override
@@ -42,60 +43,36 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(add_button);
         layout = findViewById(R.id.todo_);
 
-        read();
-        setView();
+        createFile();
 
     }
 
-    private void read(){
-        try {
-            FileInputStream fileIN = new FileInputStream(fileName);
-            ObjectInputStream objectIN = new ObjectInputStream(fileIN);
-            editList = (ArrayList<Cos_EditText>) objectIN.readObject();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void save(){
-        try {
-            FileOutputStream fileIN = new FileOutputStream(fileName);
-            ObjectOutputStream objectIN = new ObjectOutputStream(fileIN);
-            objectIN.writeObject(editList);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    private void setView(){
+    private void createFile(){
+        fileName = new File(getFilesDir(), String.valueOf(fileName));
 
         Cos_EditText cosEditText = new Cos_EditText(this,null);
         add_View(editList,cosEditText,0);
 
-
-        Log.d("数値を取得しました", String.valueOf(index));
-
-        if (index != 0){
+        if (fileName.exists()){
+            read();
             for (int i = 0;i < index; i++){
                 addTodoView(i);
                 Log.d("FileNameStart","MEMO No" + 0 + "TODO No" + i);
-
-                Cos_EditText add_EditText = new Cos_EditText(this,null);
-                add_View(editList,add_EditText,i+1);
+                }
+            for (int i = 0;i < index; i++){
+                editList.get(i).setText(content.get(i));
+                Log.d("テキストがセットされました", content.get(i));
             }
+
+            }else {
+            content.add(0,"");
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        //TodoのSave
         for (int i = 0; i < todoViewArrayList.size(); i++){
             try {
                 Log.d("保存中", String.valueOf(todoViewArrayList.get(i)));
@@ -104,26 +81,28 @@ public class MainActivity extends AppCompatActivity {
                 throw new RuntimeException(e);
             }
             Log.d("保存が成功しました", String.valueOf(todoViewArrayList.get(i)));
-
+            Log.d("保存が成功しました", String.valueOf(index));
         }
 
-        save();
+        editSave();
     }
 
     View.OnClickListener add_button = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            addTodoView(index);
+            addTodoView(todoViewArrayList.size());
         }
     };
 
     private void addTodoView(int i){
-
         TodoView add_TodoView = new TodoView(this,null ,"MEMO No" + 0 + "TODO No" + i,getSupportFragmentManager());
         add_View(todoViewArrayList,add_TodoView,i);
+
         Cos_EditText cosEditText = new Cos_EditText(this,null);
         add_View(editList,cosEditText,i + 1);
-        Log.d("ファイルが追加されました", String.valueOf(i));
+        content.add(i + 1,"");
+
+        Log.d("ファイルが追加されました", String.valueOf(index));
 
         //remove
         add_TodoView.x_button.setOnClickListener(new View.OnClickListener() {
@@ -136,31 +115,60 @@ public class MainActivity extends AppCompatActivity {
                 //Viewの削除
                 remove_view(add_TodoView, cosEditText, i);
 
-                /*
-                //idの変更
-                for (int i = add_TodoView.getId(); i < todoViewArrayList.size(); i++){
-                    todoViewArrayList.get(i).setId(i);
-                    editList.get(i).setId(i + 1);
-                    Log.d("アイテムが削除されidが変更されました", String.valueOf(i));
-                }
 
-                 */
+
             }
         });
     }
 
     private void add_View(ArrayList list,View view,int index){
         layout.addView(view);
-        list.add(index);
+        list.add(index,view);
     }
 
     private void remove_view (View todo,View edit,int index){
         layout.removeView(todo);
         layout.removeView(edit);
         todoViewArrayList.remove(index);
-        editList.remove(index + 1);
+        editList.remove(index);
     }
 
+    private void editSave(){
+        try {
+            for (int i = 0;i < editList.size();i++){
+                Log.d("editSave", String.valueOf(i + "." + editList.get(i).getText()));
+                content.set(i,String.valueOf(editList.get(i).getText()));
+                Log.d("コンテンツ保存中", String.valueOf(content.get(i)));
+            }
+            FileOutputStream fileIN = new FileOutputStream(fileName);
+            ObjectOutputStream objectIN = new ObjectOutputStream(fileIN);
+            objectIN.reset();
+            objectIN.writeObject(content);
+            objectIN.close();
+            fileIN.close();
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void read(){
+        try {
+            FileInputStream fileIN = new FileInputStream(fileName);
+            ObjectInputStream objectIN = new ObjectInputStream(fileIN);
+            content = (ArrayList<String>) objectIN.readObject();
+            index = content.size() - 1;
+            objectIN.close();
+            fileIN.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
